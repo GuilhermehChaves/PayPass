ed5dc3c76fe9fc0ee57a2456daac02faf263c18e
Guilherme Chaves <48635609+GuilhermehChaves@users.noreply.github.com>
2019-09-20 21:15:42 -0300

    Add files via upload

---
+++ src/main/java/com/example/root/techblue/Configs.java
@@ -0,0 +1,159 @@
+package com.example.root.techblue;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Configs extends AppCompatActivity {
+
+
+    private static String URL_DELETE = "http://mypentest.tk/xyz/ghi/abc/excluir.php";
+
+    List<String> opcoes;
+    ArrayAdapter<String> adaptador;
+    ListView lista;
+
+    private String id_card;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configs);
+
+
+        Bundle bundle = getIntent().getExtras();
+        id_card = bundle.getString("id");
+
+        lista = (ListView)findViewById(R.id.lista);
+        opcoes = new ArrayList<String>();
+
+
+        opcoes.add("Sobre");
+        opcoes.add("Sair");
+        opcoes.add("Excluir conta");
+
+        adaptador = new ArrayAdapter<String>(Configs.this, android.R.layout.simple_list_item_1, opcoes);
+        lista.setAdapter(adaptador);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position){
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2: delete();
+                        break;
+
+                }
+            }
+        });
+
+
+
+    }
+
+
+
+
+    private void delete(){
+
+        final String id;
+        id = id_card;
+
+
+
+
+
+
+        try {
+//String Request
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_DELETE,
+//ResponseListener
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(final String response) {
+
+                            if(response.equals("Excluido com sucesso!!")) {
+                                alert(response);
+                                final Intent intent = new Intent(getApplicationContext(), Login.class);
+                                startActivity(intent);
+                            }
+                            else{
+                                alert("Erro ao excluir conta.");
+                            }
+
+
+                        }
+                    }
+//ErrorListener
+                    , new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                    alert("Error" + error.toString());
+                    error.printStackTrace();
+                }
+            })
+                    //Abrir chaves aqui
+            {
+// digitar get -> map
+
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+//params.put(Key, value);
+                    params.put("id", id);
+                    return params;
+                }
+            };
+
+
+            MySingleton.getInstance(Configs.this).addToRequestQueue(stringRequest);
+
+
+        }catch (Exception ex){
+            Toast.makeText(getApplicationContext(), "Erro Catch" + ex.toString(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+
+
+
+private void alert(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/example/root/techblue/Registrar.java
@@ -0,0 +1,202 @@
+package com.example.root.techblue;
+
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcA;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Registrar extends AppCompatActivity {
+
+
+    private static  String URL = "http://mypentest.tk/xyz/ghi/abc/insert.php";
+
+    IntentFilter[] filters_register;
+    String[][] techs_register;
+    PendingIntent pendingIntent_register;
+    private NfcAdapter adapter_register;
+
+
+    private String card_id;
+    private EditText pass,user,confPass;
+    private Button btn_register;
+
+    private TextView aproximar;
+
+
+
+
+
+    private AlertDialog.Builder builder;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+
+
+
+        pendingIntent_register = PendingIntent.getActivity(this,0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
+
+
+        IntentFilter mifare = new IntentFilter((NfcAdapter.ACTION_TECH_DISCOVERED));
+        filters_register = new IntentFilter[]{mifare};
+        techs_register = new String[][]{new String[] {NfcA.class.getName()}};
+        adapter_register = NfcAdapter.getDefaultAdapter(this);
+
+
+        aproximar = (TextView)findViewById(R.id.aproximar);
+        user = (EditText)findViewById(R.id.user_register);
+        pass = (EditText)findViewById(R.id.senha_register);
+        confPass = (EditText)findViewById(R.id.conf_senha);
+        btn_register = (Button)findViewById(R.id.btn_register);
+
+        aproximar.setText("Aproxime seu cartão...");
+
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(pass.getText().toString().equals(confPass.getText().toString())) {
+                    insert();
+                }else{
+                    alert("Senhas não correspondentes");
+                }
+
+
+            }
+        });
+
+    }
+
+
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        byte[] id = tag.getId();
+        ByteBuffer wrapped = ByteBuffer.wrap(id);
+        wrapped.order(ByteOrder.LITTLE_ENDIAN);
+        int signedInt = wrapped.getInt();
+        long number = signedInt & 0xffffffffl;
+
+
+        card_id = Long.toString(number);
+        aproximar.setText("");
+        alert("Ok");
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter_register.enableForegroundDispatch(this,pendingIntent_register,filters_register,techs_register);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        adapter_register.disableForegroundDispatch(this);
+    }
+
+
+    protected void insert(){
+    final String id_user, usuario, senha;
+
+    id_user = card_id;
+    usuario = user.getText().toString();
+    senha = pass.getText().toString();
+
+
+
+        try {
+//String Request
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+//ResponseListener
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(final String response) {
+
+
+                            if(response.equals("ok")){
+                                alert("Registrado com sucesso!!");
+                                Intent intent = new Intent(getApplicationContext(), Login.class);
+                                startActivity(intent);
+                            }else{
+                                alert("Falha ao registrar-se");
+                            }
+
+
+                        }
+                    }
+//ErrorListener
+                    , new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                    alert("Error" + error.toString());
+                    error.printStackTrace();
+                }
+            })
+                    //Abrir chaves aqui
+            {
+// digitar get -> map
+
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+//params.put(Key, value);
+                    params.put("id_user", id_user);
+                    params.put("usuario", usuario);
+                    params.put("senha", senha);
+                    return params;
+                }
+            };
+
+
+            MySingleton.getInstance(Registrar.this).addToRequestQueue(stringRequest);
+
+
+        }catch (Exception ex){
+            Toast.makeText(getApplicationContext(), "Erro Catch" + ex.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+
+    private void alert(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+}
